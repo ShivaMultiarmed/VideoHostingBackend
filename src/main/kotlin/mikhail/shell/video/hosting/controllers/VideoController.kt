87edc47e71006022d49e30a6c17344ead1f2d03e
Михаил Шелудченко <theoutstandingme@gmail.com>
2024-12-19 00:19:03 +0700

    started implementing a player

--- src/main/kotlin/mikhail/shell/video/hosting/controllers/VideoController.kt
+++ src/main/kotlin/mikhail/shell/video/hosting/controllers/VideoController.kt
@@ -1,11 +1,13 @@
 package mikhail.shell.video.hosting.controllers
 
+import jakarta.servlet.http.HttpServletResponse
 import mikhail.shell.video.hosting.domain.ExtendedVideoInfo
 import mikhail.shell.video.hosting.domain.VideoInfo
 import mikhail.shell.video.hosting.service.VideoService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.io.File
 
 @RestController
 @RequestMapping("/api/v1/videos")
@@ -32,5 +34,17 @@ class VideoController @Autowired constructor(
         @RequestParam liking: Boolean
     ) : ResponseEntity<ExtendedVideoInfo> {
         return ResponseEntity.ok(videoService.rate(videoId, userId, liking))
+    }
+    @GetMapping("/{videoId}/play")
+    fun playVideo(
+        @PathVariable videoId: Long,
+        response: HttpServletResponse
+    ) {
+        val file = File("D:/VideoHostingStorage/videos/$videoId.mp4")
+        response.contentType = "video/mp4"
+        response.setContentLengthLong(file.length())
+        file.inputStream().use {
+            it.copyTo(response.outputStream)
+        }
     }
 }
