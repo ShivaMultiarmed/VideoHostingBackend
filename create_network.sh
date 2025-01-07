42fa27c28fa625b0ccff2a048f73818e74033e12
ShivaMultiarmed <mikhail_shell@cl155fbta3ugi4ndptv8-usus.ru-central1.internal>
2025-01-07 14:17:38 +0000

    unified whole application configuration

---
+++ create_network.sh
@@ -0,0 +1,1 @@
+docker-compose --file /home/mikhail_shell/Hosting/Application/docker-compose.yml up -d --force-recreate
--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 /home/mikhail_shell/Hosting/Application/gradlew build
-docker-compose --file /home/mikhail_shell/Hosting/Application/docker-compose.yml up -d --force-recreate
+/home/mikhail_shell/Hosting/Application/create_network.sh
