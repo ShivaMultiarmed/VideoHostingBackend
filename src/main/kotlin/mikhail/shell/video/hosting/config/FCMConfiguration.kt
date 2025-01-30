63c5ffb68290fe7b09f410a187e1ae9fc239621a
Mikhail_Shell <theoutstandingme@gmail.com>
2025-01-30 21:55:18 +0700

    fixed firebase configuration

--- src/main/kotlin/mikhail/shell/video/hosting/config/FCMConfiguration.kt
+++ src/main/kotlin/mikhail/shell/video/hosting/config/FCMConfiguration.kt
@@ -8,27 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import java.io.File
 import java.io.FileInputStream
 
 @Configuration
 class FCMConfiguration {
     @Value("\${hosting.application.path}")
-    private lateinit var PROJECT_ROOT: String
+    private lateinit var HOSTING_APPLICATION_PATH: String
     @Value("\${FCM_CONFIG}")
     private lateinit var FCM_CONFIG: String
     private val FIREBASE_APP = "hosting"
     @Bean
     fun firebaseApplication(): FirebaseApp {
-        val inputStream = FileInputStream("$PROJECT_ROOT/$FCM_CONFIG")
+        val inputStream = ClassPathResource(FCM_CONFIG).inputStream
         val firebaseOptions = FirebaseOptions.builder()
             .setCredentials(
                 GoogleCredentials.fromStream(inputStream)
             ).build()
-        return FirebaseApp.initializeApp(firebaseOptions, )
+        return FirebaseApp.initializeApp(firebaseOptions, FIREBASE_APP)
     }
     @Bean
     fun fcm(): FirebaseMessaging {
-        val app = FirebaseApp.getInstance(FIREBASE_APP)
+        val app = firebaseApplication()
         return FirebaseMessaging.getInstance(app)
     }
 }
