899934876e4556cf503e56509349e01a0b63f72d
Mikhail_Shell <theoutstandingme@gmail.com>
2026-03-07 20:16:34 +0700

    Increased max video size to 1 GB

--- src/main/kotlin/mikhail/shell/video/hosting/domain/ValidationRules.kt
+++ src/main/kotlin/mikhail/shell/video/hosting/domain/ValidationRules.kt
@@ -32,7 +32,7 @@ object ValidationRules {
     const val MAX_USERNAME_LENGTH = 50
     const val MAX_DESCRIPTION_LENGTH = 255
     const val MAX_IMAGE_SIZE = 10 * 1024 * 1024L
-    const val MAX_VIDEO_SIZE = 512 * 1024 * 1024
+    const val MAX_VIDEO_SIZE = 1024 * 1024 * 1024
     const val MIN_PASSWORD_LENGTH = 8
     const val MAX_PASSWORD_LENGTH = 20
     const val PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[^a-zA-Z0-9])\\S{$MIN_PASSWORD_LENGTH,$MAX_PASSWORD_LENGTH}$"
